297f79ad28f0da7e24f7e3928814a5329d9a556b
Cameron Pittman <cameron.w.pittman@gmail.com>
2015-03-30 14:43:30 -0700

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -427,7 +427,6 @@ var resizePizzas = function(size) {
     var windowwidth = document.querySelector("#randomPizzas").offsetWidth;
     var oldsize = oldwidth / windowwidth;
 
-    // TODO: change to 3 sizes? no more xl?
     // Changes the slider value to a percent width
     function sizeSwitcher (size) {
       switch(size) {
